f6bc4be187edbed715ec87c33f64aec3893bdad7
Chad Lei <cblei@uci.edu>
2018-02-15 15:58:13 -0800

    GOT VUE TO WORK

---
+++ vue.js
@@ -0,0 +1,20 @@
+// var nav = new Vue({  
+// 	el: '#fake-nav',
+// 	methods: {
+// 		open: function(which, e) {
+// 			// Prevents clicking the link from doing anything
+// 			e.preventDefault();
+// 			$('#login-modal').addClass('active');
+// 		}
+// 	}
+// });
+
+// var modal = new Vue({  
+// 	el: '#login-modal',
+// 	methods: {
+// 		close: function(e) {
+// 	  		e.preventDefault();
+// 	  		$('#login-modal').removeClass('active');
+// 		}
+// 	}
+// });
