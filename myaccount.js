4818012a4a0b8372a7a93dbe33b77f8e5599db5a
phucmp <phucmp@users.noreply.github.com>
2018-02-15 17:40:49 -0800

    Finished Profile Page and started myaccount page
    Added in some graphics as well

---
+++ myaccount.js
@@ -0,0 +1,24 @@
+// My Account JavaScript File
+
+var info = new Vue({
+  el: '#info',
+  data: {
+    name: 'CHAD LEI',
+    address: '1038 Arroyo Drive, Irvine, CA 92617',
+    number: '949.120.3569',
+    service: 'Barber',
+    summary: 'I am a fourth year student who studies computer science but have a passion for cutting hair',
+    fblink: 'https://www.facebook.com/',
+    iglink: 'https://www.instagram.com/',
+    profilePic: 'tempprofile.png'
+  }
+})
+
+var portfolio = new Vue({
+  el: '#portfolioPics',
+  data: {
+    pic1: 'samp1.jpeg',
+    pic2: 'samp2.jpg',
+    pic3: 'samp3.png'
+  }
+})
--- profile.js
+++ profile.js
@@ -24,4 +24,46 @@ function initMap() {
     position: location,
     map: map
   });
-} 
+}
+
+var pname ='CHAD LEI';
+// Vue.js Code
+
+var profileInfo = new Vue({
+  el: '#providerInfo',
+  data: {
+    name: pname,
+    address: '1038 Arroyo Drive, Irvine, CA 92617',
+    number: '949.120.3569',
+    service: 'Barber',
+    summary: 'I am a fourth year student who studies computer science but have a passion for cutting hair',
+    fblink: 'https://www.facebook.com/',
+    iglink: 'https://www.instagram.com/',
+    profilePic: 'tempprofile.png'
+  }
+})
+
+var portfolio = new Vue({
+  el: '#portfolioPics',
+  data: {
+    pic1: 'samp1.jpeg',
+    pic2: 'samp2.jpg',
+    pic3: 'samp3.png'
+  }
+})
+
+/* 
+// REVIEW SYSTEM
+
+var rating = new Vue({
+  el: '#rating',
+  data: {
+    emoji: '4.png',
+    e1: '1.png',
+    e2: '2.png',
+    e3: '3.png',
+    e4: '4.png',
+    e5: '5.png'
+  }
+})
+*/
