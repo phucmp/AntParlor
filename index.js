7969f3b9c7d536d487824b28ec00b5a71a6aacb8
Ricardo <GarciaR@protonmail.com>
2018-03-17 00:38:47 -0700

    priceAndTypeButtons
    Added buttons for recommendation on price and amateur/professional. Doesn't display info yet, just has info in the console.

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-var lat, lng, latlng;
+var lat, lng, latlng,currentCity;
+document.cookie = "professional"
+
+// AWS ~~~~~~~~~
+// Configure AWS SDK for JavaScript for LAMBDA
+AWS.config.region = 'us-west-2'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-west-2:01df0e0c-6be5-46cd-b277-f60d4e3021a0',
+});
+
+// Prepare to call Lambda function
+var lambda = new AWS.Lambda({region: 'us-west-1', apiVersion: '2015-03-31'});
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -37,7 +48,8 @@ function geocodeLatLng() {
     geocoder.geocode({'location': latlng}, function(results, status) {
       if (status === 'OK') {
         if (results[0]) {
-       		console.log(results[0].formatted_address);
+               console.log(results[0].formatted_address);
+               currentCity = results[0].address_components[2].long_name
         } else {
           window.alert('No results found');
         }
@@ -47,6 +59,76 @@ function geocodeLatLng() {
     });
 }
 
+function switchType(professional){
+    if (professional == "professional"){
+        document.cookie = "professional";
+        
+    }
+    else if(professional == "amateur"){
+        document.cookie = "amateur";
+
+    }
+}
+
+function testPriceFunc(price,t){
+    var professional = document.cookie;
+    console.log(professional);
+    if (currentCity == null){
+        currentCity = "Irvine"
+    }
+    if (professional == 'professional' || professional == null){
+        console.log('a');
+        var pullParams = {
+            FunctionName : 'priceRec',
+            InvocationType : 'RequestResponse',
+            LogType : 'None',
+            Payload: '{"city" : "'+ currentCity +'", "action" : "query"}'
+    
+          };
+    }
+    if (professional == 'amateur'){
+        console.log('b');
+        var pullParams = {
+            FunctionName : 'priceRecStudents',
+            InvocationType : 'RequestResponse',
+            LogType : 'None',
+            Payload: '{"city" : "'+ currentCity +'", "action" : "query"}'
+    
+          };
+    }
+
+    lambda.invoke(pullParams, function(error, data) {
+        if (error) {
+          prompt(error);
+        } else {
+          pullResults = JSON.parse(data.Payload).Items;
+          if(price == "$"){
+            for (var key in pullResults) {
+                if(pullResults[key].price == '$'){
+                  console.log(pullResults[key]);
+              }
+          }
+          }
+          else if(price == "$$"){
+            for (var key in pullResults) {
+                if(pullResults[key].price == '$$'){
+                  console.log(pullResults[key]);
+              }
+          }
+          }
+          else if(price == "$$$"){
+            for (var key in pullResults) {
+                if(pullResults[key].price == '$$$'){
+                  console.log(pullResults[key]);
+              }
+          }
+
+          }
+          return pullResults;
+    }
+});
+}
+    
 
 $( document ).ready(function() {
 	getLocation();
